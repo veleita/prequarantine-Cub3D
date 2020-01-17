bde3ad156fdbfb2f2db5248f4d7f6e2fd0bbaa97
Maria Zomeno gutierrez <mzomeno-@c3r14s3.42madrid.com>
2020-01-17 16:19:46 +0100

    printing lines and simple shapes on a window

--- mlx_testing.c
+++ mlx_testing.c
@@ -1,12 +1,57 @@
 #include <mlx.h>
 #include <stdio.h>
+#include <math.h>
 
-int main()
+typedef struct  s_data {
+    void        *img;
+    char        *addr;
+    int         bits_per_pixel;
+    int         line_length;
+    int         endian;
+}               t_data;
+
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	void *mlx;
+    char    *dst;
 
-	mlx = mlx_init();
-	mlx_new_window(mlx, 500, 500, "Hello world!");
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+int             main(void)
+{
+    void    *mlx;
+    void    *mlx_win;
+	t_data  img;
+//	int		y;
+//	int		x;
+
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+    img.img = mlx_new_image(mlx, 500, 500);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								 &img.endian);
+/*
+ *---------------------------PRINT A SQUARE------------------------------------
+ * 
+ *	while (250 - x == 250 - y)
+ *		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+ *	while (x++ < 250)
+ *		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+ *	while (y-- > 100)
+ *		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+ *	while (x-- > 100)
+ *		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+ */
+/*
+ * --------------------------PRINT A CIRCLE------------------------------------
+ *	while (x++ < 200)
+ *	{
+ *		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+ *		y = sqrt(x*x + 2*x + 8) - 2;
+ *	}
+ */
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
 }
