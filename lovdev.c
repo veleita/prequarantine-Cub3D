c871b264f1a1debf04f23dab1d2061a2d97ada71
Maria Zomeno gutierrez <mzomeno-@c2r11s4.42madrid.com>
2020-01-22 18:18:44 +0100

    wallheight calculated

--- lovdev.c
+++ lovdev.c
@@ -1,94 +1,141 @@
+#include <math.h>
+
 #define mapWidth 24
 #define mapHeight 24
 #define screenWidth 640
 #define screenHeight 480
+#define wallHeight 400
 
 int worldMap[mapWidth][mapHeight]=
 {
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
 int main(int argc, char *argv[])
 {
-  double posX = 22, posY = 12;  //x and y start position
-  double dirX = -1, dirY = 0; //initial direction vector
-  double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+	double posX = 22, posY = 12;  //x and y start position
+	double dirX = -1, dirY = 0; //initial direction vector
+	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
 
-  double time = 0; //time of current frame
-  double oldTime = 0; //time of previous frame
-  mlx_new_window(var.mlx, screenWidth, screenHeight, "Raycaster");
+	double time = 0; //time of current frame
+	double oldTime = 0; //time of previous frame
+	mlx_new_window(var.mlx, screenWidth, screenHeight, "Raycaster");
 
 	//which box of the map we're in
 	int mapX = int(posX);
-    int mapY = int(posY);
+	int mapY = int(posY);
 
 	//length of ray from current position to next x or y-side
-    double sideDistX;
-    double sideDistY;
+	double sideDistX;
+	double sideDistY;
 
-     //length of ray from one x or y-side to next x or y-side
-    double deltaDistX = std::abs(1 / rayDirX);
-    double deltaDistY = std::abs(1 / rayDirY);
-    double perpWallDist;
 
-    //what direction to step in x or y-direction (either +1 or -1)
-    int stepX;
-    int stepY;
+	//what direction to step in x or y-direction (either +1 or -1)
+	int stepX;
+	int stepY;
 
-    int hit = 0; //was there a wall hit?
-    int side; //was a NS or a EW wall hit?
+	int hit = 0; //was there a wall hit?
+	int side; //was a NS or a EW wall hit?
 
-  while(!done())
-  {
-	  int x = 0;
-	  while (x < w)
-	  {
-		//calculate ray position and direction
-		double cameraX = 2 * x / double(w) - 1; //x-coordinate in camera space
-		double rayDirX = dirX + planeX * cameraX;
-		double rayDirY = dirY + planeY * cameraX;
-		//calculate step and initial sideDist
-		if (rayDirX < 0)
+	while(!done())
+	{
+		int x = 0;
+		while (x < w)
 		{
-			stepX = -1;
-			sideDistX = (posX - mapX) * deltaDistX;//posX is a double, mapX an 
-		}//											int, so pos - map is 0.n (hacer dibujito del triángulo)
-		else
-		{
-			stepX = 1;
-			sideDistX = (mapX + 1.0 - posX) * deltaDistX;//with this, we've got
-		}//												the "oposite" value, if
-		if (rayDirY < 0)//								we had 0.2 above, here
-		{//												it is 0.8
-			stepY = -1;
-			sideDistY = (posY - mapY) * deltaDistY;
-		}
-		else
-		{
-			stepY = 1;
-			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-		}
-		//To be continued...
+			//calculate ray position and direction
+			double cameraX = 2 * x / double(w) - 1; //x-coordinate in camera space
+			double rayDirX = dirX + planeX * cameraX;
+			double rayDirY = dirY + planeY * cameraX;
+
+			//length of ray from one x or y-side to next x or y-side
+			//			HAY QUE CONTROLAR PARA QUE CUANDO DIVIDA ENTRE 0 EL COCIENTE
+			//			DE 0 AUTOMÁTICAMENTE (FLOATING POINT EXCEPTION)
+			double deltaDistX = rayDirX == 0 ? 0 : sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+			double deltaDistY = rayDirY == 0 ? 0 : sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+			double perpWallDist;
+
+			//calculate step and initial sideDist
+			if (rayDirX < 0)
+			{
+				stepX = -1;
+				sideDistX = (posX - mapX) * deltaDistX;//posX is a double, mapX an 
+			}//											int, so pos - map is 0.n
+			else
+			{
+				stepX = 1;
+				sideDistX = (mapX + 1.0 - posX) * deltaDistX;//with this, we've got
+			}//												the "oposite" value, if
+			if (rayDirY < 0)//								we had 0.2 above, here
+			{//												it is 0.8
+				stepY = -1;
+				sideDistY = (posY - mapY) * deltaDistY;
+			}
+			else
+			{
+				stepY = 1;
+				sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+			}
+			//perform DDA
+			while (hit == 0)
+			{
+				//jump to next map square, OR in x-direction, OR in y-direction
+				if (sideDistX < sideDistY)
+				{
+					sideDistX += deltaDistX;
+					mapX += stepX;
+					side = 0;
+				}
+				else
+				{
+					sideDistY += deltaDistY;
+					mapY += stepY;
+					side = 1;
+				}
+				//Check if ray has hit a wall
+				hit = worldMap[mapX][mapY] > 0 ? 1 : 0;
+			}
+
+			//Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+			if (side == 0)
+				perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+			else
+				perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+
+			//Calculate height of line to draw on screen
+			int lineHeight = (int)(wallHeight / perpWallDist);
+
+			//Calculate lowest and highest pixel to fill in current stripe
+			int drawStart = wallHeight / 2 - lineHeight / 2;
+
+			if(drawStart < 0)//I don't really understand why you need to check this
+				drawStart = 0;
+			
+			int drawEnd = wallHeight / 2 + lineHeight / 2;
+
+			if(drawEnd >= wallHeight)//Same as before, is this condition ever true?
+				drawEnd = wallHeight - 1;
+
+			//choose wall color.....To be continued.
